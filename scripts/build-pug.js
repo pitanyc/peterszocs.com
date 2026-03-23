fb0823229f6be39191bb26feabcca298e77983be
Peter Szocs <pszocs@bloomberg.net>
2026-03-22 22:12:31 -0700

    Music fixes

--- scripts/build-pug.js
+++ scripts/build-pug.js
@@ -5,7 +5,14 @@ const renderPug = require('./render-pug');
 
 const srcPath = upath.resolve(upath.dirname(__filename), '../src');
 
+const promises = [];
+
 sh.find(srcPath).forEach(_processFile);
+
+Promise.all(promises).catch(err => {
+    console.error('Error building pug files:', err);
+    process.exit(1);
+});
 
 function _processFile(filePath) {
     if (
@@ -14,6 +21,6 @@ function _processFile(filePath) {
         && !filePath.match(/mixin/)
         && !filePath.match(/\/pug\/layouts\//)
     ) {
-        renderPug(filePath);
+        promises.push(renderPug(filePath));
     }
 }
